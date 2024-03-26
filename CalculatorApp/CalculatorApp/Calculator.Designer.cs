d0f681095bc7b215865530b2aaa95de639181cd3
nguyenhieuhien <nguyenhieuhiendevse@gmail.com>
2024-03-26 19:23:03 +0700

    Upload Calculator Form

---
+++ CalculatorApp/CalculatorApp/Calculator.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace CalculatorApp
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            btnEqual = new Button();
+            btnAdd = new Button();
+            btnSub = new Button();
+            btnMul = new Button();
+            btnDiv = new Button();
+            btnDot = new Button();
+            btnNum0 = new Button();
+            btnNum9 = new Button();
+            btnNum6 = new Button();
+            btnNum3 = new Button();
+            btnNum2 = new Button();
+            btnNum5 = new Button();
+            btnNum8 = new Button();
+            btnNum1 = new Button();
+            btnNum4 = new Button();
+            btnNum7 = new Button();
+            btnPercent = new Button();
+            btnFraction = new Button();
+            btnSquareRoot = new Button();
+            btnSquare = new Button();
+            btnDel1Char = new Button();
+            btnDelAll = new Button();
+            btnDelEntry = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(481, 86);
+            textBox1.TabIndex = 0;
+            // 
+            // btnEqual
+            // 
+            btnEqual.Location = new Point(415, 494);
+            btnEqual.Name = "btnEqual";
+            btnEqual.Size = new Size(94, 69);
+            btnEqual.TabIndex = 1;
+            btnEqual.Text = "=";
+            btnEqual.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(415, 419);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 69);
+            btnAdd.TabIndex = 2;
+            btnAdd.Text = "+";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnSub
+            // 
+            btnSub.Location = new Point(415, 344);
+            btnSub.Name = "btnSub";
+            btnSub.Size = new Size(94, 69);
+            btnSub.TabIndex = 3;
+            btnSub.Text = "-";
+            btnSub.UseVisualStyleBackColor = true;
+            // 
+            // btnMul
+            // 
+            btnMul.Location = new Point(415, 269);
+            btnMul.Name = "btnMul";
+            btnMul.Size = new Size(94, 69);
+            btnMul.TabIndex = 4;
+            btnMul.Text = "x";
+            btnMul.UseVisualStyleBackColor = true;
+            // 
+            // btnDiv
+            // 
+            btnDiv.Location = new Point(415, 194);
+            btnDiv.Name = "btnDiv";
+            btnDiv.Size = new Size(94, 69);
+            btnDiv.TabIndex = 5;
+            btnDiv.Text = "÷";
+            btnDiv.UseVisualStyleBackColor = true;
+            // 
+            // btnDot
+            // 
+            btnDot.Location = new Point(315, 494);
+            btnDot.Name = "btnDot";
+            btnDot.Size = new Size(94, 69);
+            btnDot.TabIndex = 6;
+            btnDot.Text = ".";
+            btnDot.UseVisualStyleBackColor = true;
+            // 
+            // btnNum0
+            // 
+            btnNum0.Location = new Point(215, 494);
+            btnNum0.Name = "btnNum0";
+            btnNum0.Size = new Size(94, 69);
+            btnNum0.TabIndex = 7;
+            btnNum0.Text = "0";
+            btnNum0.UseVisualStyleBackColor = true;
+            // 
+            // btnNum9
+            // 
+            btnNum9.Location = new Point(315, 419);
+            btnNum9.Name = "btnNum9";
+            btnNum9.Size = new Size(94, 69);
+            btnNum9.TabIndex = 8;
+            btnNum9.Text = "9";
+            btnNum9.UseVisualStyleBackColor = true;
+            // 
+            // btnNum6
+            // 
+            btnNum6.Location = new Point(315, 344);
+            btnNum6.Name = "btnNum6";
+            btnNum6.Size = new Size(94, 69);
+            btnNum6.TabIndex = 9;
+            btnNum6.Text = "6";
+            btnNum6.UseVisualStyleBackColor = true;
+            // 
+            // btnNum3
+            // 
+            btnNum3.Location = new Point(315, 269);
+            btnNum3.Name = "btnNum3";
+            btnNum3.Size = new Size(94, 69);
+            btnNum3.TabIndex = 10;
+            btnNum3.Text = "3";
+            btnNum3.UseVisualStyleBackColor = true;
+            // 
+            // btnNum2
+            // 
+            btnNum2.Location = new Point(215, 269);
+            btnNum2.Name = "btnNum2";
+            btnNum2.Size = new Size(94, 69);
+            btnNum2.TabIndex = 11;
+            btnNum2.Text = "2";
+            btnNum2.UseVisualStyleBackColor = true;
+            // 
+            // btnNum5
+            // 
+            btnNum5.Location = new Point(215, 344);
+            btnNum5.Name = "btnNum5";
+            btnNum5.Size = new Size(94, 69);
+            btnNum5.TabIndex = 12;
+            btnNum5.Text = "5";
+            btnNum5.UseVisualStyleBackColor = true;
+            // 
+            // btnNum8
+            // 
+            btnNum8.Location = new Point(215, 419);
+            btnNum8.Name = "btnNum8";
+            btnNum8.Size = new Size(94, 69);
+            btnNum8.TabIndex = 13;
+            btnNum8.Text = "8";
+            btnNum8.UseVisualStyleBackColor = true;
+            // 
+            // btnNum1
+            // 
+            btnNum1.Location = new Point(115, 269);
+            btnNum1.Name = "btnNum1";
+            btnNum1.Size = new Size(94, 69);
+            btnNum1.TabIndex = 14;
+            btnNum1.Text = "1";
+            btnNum1.UseVisualStyleBackColor = true;
+            // 
+            // btnNum4
+            // 
+            btnNum4.Location = new Point(115, 344);
+            btnNum4.Name = "btnNum4";
+            btnNum4.Size = new Size(94, 69);
+            btnNum4.TabIndex = 15;
+            btnNum4.Text = "4";
+            btnNum4.UseVisualStyleBackColor = true;
+            // 
+            // btnNum7
+            // 
+            btnNum7.Location = new Point(115, 419);
+            btnNum7.Name = "btnNum7";
+            btnNum7.Size = new Size(94, 69);
+            btnNum7.TabIndex = 16;
+            btnNum7.Text = "7";
+            btnNum7.UseVisualStyleBackColor = true;
+            // 
+            // btnPercent
+            // 
+            btnPercent.Location = new Point(115, 494);
+            btnPercent.Name = "btnPercent";
+            btnPercent.Size = new Size(94, 69);
+            btnPercent.TabIndex = 17;
+            btnPercent.Text = "%";
+            btnPercent.UseVisualStyleBackColor = true;
+            // 
+            // btnFraction
+            // 
+            btnFraction.Location = new Point(15, 344);
+            btnFraction.Name = "btnFraction";
+            btnFraction.Size = new Size(94, 69);
+            btnFraction.TabIndex = 19;
+            btnFraction.Text = "1/x";
+            btnFraction.UseVisualStyleBackColor = true;
+            // 
+            // btnSquareRoot
+            // 
+            btnSquareRoot.Location = new Point(15, 419);
+            btnSquareRoot.Name = "btnSquareRoot";
+            btnSquareRoot.Size = new Size(94, 69);
+            btnSquareRoot.TabIndex = 20;
+            btnSquareRoot.Text = "√x";
+            btnSquareRoot.UseVisualStyleBackColor = true;
+            // 
+            // btnSquare
+            // 
+            btnSquare.Location = new Point(15, 494);
+            btnSquare.Name = "btnSquare";
+            btnSquare.Size = new Size(94, 69);
+            btnSquare.TabIndex = 21;
+            btnSquare.Text = "x²";
+            btnSquare.UseVisualStyleBackColor = true;
+            // 
+            // btnDel1Char
+            // 
+            btnDel1Char.Location = new Point(415, 119);
+            btnDel1Char.Name = "btnDel1Char";
+            btnDel1Char.Size = new Size(94, 69);
+            btnDel1Char.TabIndex = 22;
+            btnDel1Char.Text = "C";
+            btnDel1Char.UseVisualStyleBackColor = true;
+            // 
+            // btnDelAll
+            // 
+            btnDelAll.Location = new Point(315, 119);
+            btnDelAll.Name = "btnDelAll";
+            btnDelAll.Size = new Size(94, 69);
+            btnDelAll.TabIndex = 23;
+            btnDelAll.Text = "AC";
+            btnDelAll.UseVisualStyleBackColor = true;
+            // 
+            // btnDelEntry
+            // 
+            btnDelEntry.Location = new Point(215, 119);
+            btnDelEntry.Name = "btnDelEntry";
+            btnDelEntry.Size = new Size(94, 69);
+            btnDelEntry.TabIndex = 24;
+            btnDelEntry.Text = "CE";
+            btnDelEntry.UseVisualStyleBackColor = true;
+            // 
+            // Calculator
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(528, 575);
+            Controls.Add(btnDelEntry);
+            Controls.Add(btnDelAll);
+            Controls.Add(btnDel1Char);
+            Controls.Add(btnSquare);
+            Controls.Add(btnSquareRoot);
+            Controls.Add(btnFraction);
+            Controls.Add(btnPercent);
+            Controls.Add(btnNum7);
+            Controls.Add(btnNum4);
+            Controls.Add(btnNum1);
+            Controls.Add(btnNum8);
+            Controls.Add(btnNum5);
+            Controls.Add(btnNum2);
+            Controls.Add(btnNum3);
+            Controls.Add(btnNum6);
+            Controls.Add(btnNum9);
+            Controls.Add(btnNum0);
+            Controls.Add(btnDot);
+            Controls.Add(btnDiv);
+            Controls.Add(btnMul);
+            Controls.Add(btnSub);
+            Controls.Add(btnAdd);
+            Controls.Add(btnEqual);
+            Controls.Add(textBox1);
+            Name = "Calculator";
+            Text = "Calculator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button btnEqual;
+        private Button btnAdd;
+        private Button btnSub;
+        private Button btnMul;
+        private Button btnDiv;
+        private Button btnDot;
+        private Button btnNum0;
+        private Button btnNum9;
+        private Button btnNum6;
+        private Button btnNum3;
+        private Button btnNum2;
+        private Button btnNum5;
+        private Button btnNum8;
+        private Button btnNum1;
+        private Button btnNum4;
+        private Button btnNum7;
+        private Button btnPercent;
+        private Button btnFraction;
+        private Button btnSquareRoot;
+        private Button btnSquare;
+        private Button btnDel1Char;
+        private Button btnDelAll;
+        private Button btnDelEntry;
+    }
+}
